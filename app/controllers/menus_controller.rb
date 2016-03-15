6bc141c366d213eb1e716fcdc7d53d3b773a4493
Rachel Guerin <rachel.guerin23@gmail.com>
2016-03-15 23:17:30 +0100

    formatting menu cards

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,6 +1,10 @@
 class MenusController < ApplicationController
 
 	def index
-		
+		@monday_recipe = Recipe.where("title like 'E%'").first
+		@tuesday_recipe = Recipe.where("title like 'T%'").first
+		@wednesday_recipe = Recipe.where("title like 'G%'").first
+		@thursday_recipe = Recipe.where("title like 'H%'").last
+		@friday_recipe = Recipe.fifth
 	end
 end
