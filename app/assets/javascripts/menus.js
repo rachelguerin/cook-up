844056764e41e10da812e04ed8408890e05143af
Rachel Guerin <rachel.guerin23@gmail.com>
2016-03-16 14:25:08 +0100

    first modal js-time-limit

--- app/assets/javascripts/menus.js
+++ app/assets/javascripts/menus.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on("ready",function(){
+	$('.js-go').on('click',function(){
+		$('.js-time-limit').addClass('is-active');
+	});
+
+	$('.modal-background, .modal-close').on('click',function(){
+		$('.modal').removeClass('is-active');
+	});
+});
