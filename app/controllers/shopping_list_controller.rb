385191b54f1bee6ff81ab3b4a804909526cd623f
Rachel Guerin <rachel.guerin23@gmail.com>
2016-03-22 12:43:14 +0100

    add partial to shopping list

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,5 +1,14 @@
 class ShoppingListController < ApplicationController
 	def list
+		condiments = ["Spices and Seasonings","Oil, Vinegar, Salad Dressing","Ethnic Foods","Condiments"]
 		@shopping_list = Recipe.get_shopping_list(params[:menu])
+		@shopping_list_condiments = @shopping_list.select do |k,v| 
+			condiments.include?(k)
+		end
+
+		@shopping_list.select! do |k,v| 
+			!condiments.include?(k)
+		end
+		
 	end
 end
